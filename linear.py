886e26928c124a4cd31640067f1c362f896dae1b
Wilder Fulford <fulford.wilder@gmail.com>
2023-03-15 00:24:51 -0700

    linear

--- linear.py
+++ linear.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 import re
-
+import matplotlib.pyplot as plt
+import random
 
 def preprocess_data(filepath):
     """
@@ -41,7 +43,7 @@ def preprocess_data(filepath):
         'Atorvastatin (Lipitor)', 'Fluvastatin (Lescol)', 'Pravastatin (Pravachol)', 'Rosuvastatin (Crestor)', 
         'Cerivastatin (Baycol)', 'Amiodarone (Cordarone)', 'Carbamazepine (Tegretol)', 'Phenytoin (Dilantin)', 
         'Rifampin or Rifampicin', 'Sulfonamide Antibiotics', 'Macrolide Antibiotics', 'Anti-fungal Azoles', 
-        'Herbal Medications, Vitamins, Supplements', 'Target INR', 'Current Smoker'
+        'Herbal Medications, Vitamins, Supplements', 'Target INR', 'Current Smoker', 'Therapeutic Dose of Warfarin'
     ]
 
     # Extract the relevant columns from the dataframe
@@ -122,19 +124,129 @@ def preprocess_data(filepath):
     
     feature_df = pd.get_dummies(feature_df, columns=categorical_columns_to_encode)
     
-    # Shuffle the preprocessed dataframe
-    feature_df = feature_df.sample(frac=1).reset_index(drop=True)
     # Yield feature vectors one at a time
     for index, row in feature_df.iterrows():
-        yield row.to_numpy()
-        
-        
+        dose = row['Therapeutic Dose of Warfarin']
+        row = row.drop('Therapeutic Dose of Warfarin')
 
-i = 0
-for row in preprocess_data('data/warfarin.csv'):
-    print(row)
-    print(row.size)
-    i += 1
-    if i == 10:
-        break
+        yield row.to_numpy(), dose
+   
+        
+def patient_generator(file_path):
+    preprocessor = preprocess_data(file_path)
+    patients = list(preprocessor)
+    random.shuffle(patients)
+    for patient in patients:
+        yield patient
+
+
+class LinearUCB:
+    def __init__(self, d, K, alpha):
+        self.d = d
+        self.K = K
+        self.alpha = alpha
+        self.A = [np.identity(d) for _ in range(K)]
+        self.b = [np.zeros(d) for _ in range(K)]
+        self.theta = [np.zeros(d) for _ in range(K)]
+
+    def choose_action(self, x):
+        ucb_values = []
+        for i in range(self.K):
+            self.theta[i] = np.linalg.inv(self.A[i]).dot(self.b[i])
+            mean = x.T.dot(self.theta[i])
+            uncertainty = self.alpha * np.sqrt(x.T.dot(np.linalg.inv(self.A[i])).dot(x))
+            ucb_values.append(mean + uncertainty)
+        return np.argmax(ucb_values)
+
+    def update(self, arm, x, reward):
+        self.A[arm] += x.dot(x.T)
+        self.b[arm] += reward * x
+
+
+def simulate_bandit(file_path, num_patients, feature_dim, num_actions, alpha):
+    generator = patient_generator(file_path)
+    bandit = LinearUCB(feature_dim, num_actions, alpha)
+    regret = 0
+    incorrect_dosing_decisions = 0
+    total_reward = 0
+
+    for t in range(num_patients):
+        patient_features, therapeutic_dose = next(generator)
+        action = bandit.choose_action(patient_features)
+        
+        if therapeutic_dose < 21:
+            best_action = 0
+        elif therapeutic_dose <= 49:
+            best_action = 1
+        else:
+            best_action = 2
+
+        reward = -1 if action != best_action else 0
+        total_reward += reward
+        incorrect_dosing_decisions += (reward == -1)
+        regret -= total_reward
+        fraction_incorrect = incorrect_dosing_decisions / (t + 1)
+        bandit.update(action, patient_features, reward)
+
+    return regret, fraction_incorrect
+
+
+def run_bandit_multiple_times(file_path, num_patients, feature_dim, num_actions, alpha, num_runs=20):
+    regrets = []
+    fractions_incorrect = []
+
+    for _ in range(num_runs):
+
+        regret, fraction_incorrect = simulate_bandit(file_path, num_patients, feature_dim, num_actions, alpha)
+        regrets.append(regret)
+        fractions_incorrect.append(fraction_incorrect)
+
+    # Calculate average performance
+    avg_regret = np.mean(regrets)
+    avg_fraction_incorrect = np.mean(fractions_incorrect)
+
+    # Calculate 95% confidence intervals
+    t_score = stats.t.ppf(1 - 0.025, num_runs - 1)  # T-distribution
+
+    stderr_regret = stats.sem(regrets)
+    ci_regret = t_score * stderr_regret
+
+    stderr_fraction_incorrect = stats.sem(fractions_incorrect)
+    ci_fraction_incorrect = t_score * stderr_fraction_incorrect
+
+    return (avg_regret, ci_regret), (avg_fraction_incorrect, ci_fraction_incorrect)
+
+
+def plot_performance(file_path, num_patients, feature_dim, num_actions, alpha, num_runs=20):
+    (avg_regret, ci_regret), (avg_fraction_incorrect, ci_fraction_incorrect) = run_bandit_multiple_times(
+        file_path, num_patients, feature_dim, num_actions, alpha, num_runs
+    )
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Plot Regret performance
+    ax[0].bar(1, avg_regret, yerr=ci_regret, capsize=10)
+    ax[0].set_title("Regret Performance")
+    ax[0].set_ylabel("Average Regret")
+    ax[0].set_xticks([])
+
+    # Plot Fraction Incorrect performance
+    ax[1].bar(1, avg_fraction_incorrect, yerr=ci_fraction_incorrect, capsize=10)
+    ax[1].set_title("Fraction Incorrect Performance")
+    ax[1].set_ylabel("Average Fraction Incorrect")
+    ax[1].set_xticks([])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Parameters
+    num_patients = 5528
+    feature_dim = 31
+    num_actions = 3  # Low, medium, high
+    alpha = 2
+    file_path = "data/warfarin.csv"
+    
+    plot_performance(file_path, num_patients, feature_dim, num_actions, alpha)
+
     
